ed18740ac9864a6d4f2fc860d52b21ca4abb7153
sunainaasif <saad.razi@digitonics.com>
2020-11-18 19:22:44 +0500

    3rd commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,4 +3,14 @@ $(document).ready(function () {
     $(".header").toggleClass("header-mbl");
     $("body").toggleClass("fixed-position");
   });
+  $('.testimonials-slider').slick({
+    arrows:true,
+    
+  });
+  
 });
+
+
+// $(document).ready(function(){
+//   $('.testimonials-slider').slick();
+// });

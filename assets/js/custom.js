fe3fce3ff2d5673d8b87f8c7c8ef2b1efe21f90c
sunainaasif <saad.razi@digitonics.com>
2020-11-26 19:37:21 +0500

    fixes 2

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,11 +12,39 @@ $(document).ready(function () {
   });
   $(".testimonials-slider").slick({
     arrows: true,
-    // autoplay: true,
-    // autoplaySpeed: 2500,
+    autoplay: true,
+    autoplaySpeed: 3000,
   });
 });
 
-// $(document).ready(function(){
-//   $('.testimonials-slider').slick();
-// });
+// To stick header //
+$(document).ready( function() {
+        
+  $(".header_container").css('background','transparent'); //hide your div initially
+  var topOfOthDiv = $("#special-section").offset().top;
+  $(window).scroll(function() {
+      if($(window).scrollTop() > topOfOthDiv - 200) { //scrolled past the other div?
+          $(".header_container").css('background','linear-gradient(rgb(233 214 135), #b4913f)'); //reached the desired point -- show div
+      
+          $(".header_container").css('position','fixed');
+          $(".header_container").css('width','100%');
+          $(".header_container").css('height:','100px');
+          
+
+          
+
+          // $(".lines div").css('background','#c5a552');
+          if(!$('.header').hasClass('header-mbl'))
+          $(".lines div").css('background','#c5a552');
+      }else {
+          $(".header_container").css('background','transparent'); //hide your div initially
+          $(".header_container").css('position','static');
+          $(".header_container").css('width','100%');
+          
+
+          $(".lines div").css('background','#fff');
+      }
+  });
+});
+
+// --------////

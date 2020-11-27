fac4d459d320185dfe4629175c610fe37aab78cd
sunainaasif <saad.razi@digitonics.com>
2020-11-27 19:53:17 +0500

    Scroll top & fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,9 +12,16 @@ $(document).ready(function () {
   });
   $(".testimonials-slider").slick({
     arrows: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
+    // autoplay: true,
+    // autoplaySpeed: 3000,
   });
+  $(".slick-next").addClass('selected')
+  $(".slick-prev , .slick-next").click(function() {
+    $(this).addClass('selected').siblings().removeClass('selected');
+    // $(this).addClass('selected').siblings().removeClass('selected');
+
+  
+    });
 });
 
 // To stick header //
@@ -48,3 +55,24 @@ $(document).ready( function() {
 });
 
 // --------////
+// Scroll to top //
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+// --------------------//
